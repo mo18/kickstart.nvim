b6936edb5ec60fb9bec94b3a3220febc1f544d0d
Mario Scubla <mario@scubla.net>
2025-01-12 10:46:58 -0500

    update defaults and add some plugins

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,37 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  -- version = '*',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<C-n>', ':Neotree filesystem toggle left<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>nr', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enable = true,
+        leave_dirs_open = false,
+      },
+      group_empty_dirs = true,
+      filtered_items = {
+        hide_dotfiles = true,
+      },
+      window = {
+        width = 50,
+        side = 'left',
+        mappings = {
+          ['<C-n>'] = 'close_window',
+        },
+      },
+    },
+  },
+}
